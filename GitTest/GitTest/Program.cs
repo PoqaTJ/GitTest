b15491c59f91872de45343c05312340132105367
PoqaTJ <Tbjohnson42@gmail.com>
2013-11-11 19:45:36 -0800

    Random string appears after "Hello World"
    Add to list strings!

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -9,7 +9,13 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            List<String> strings = new List<String>() { "Git over here!", "The Gittysburg Address", "Gitar Hero" };
+            Random rand = new Random();
+
             Console.WriteLine("Hello, world!");
+            Console.WriteLine();
+
+            Console.WriteLine(strings[rand.Next(strings.Count)]);
             Console.ReadLine();
         }
     }

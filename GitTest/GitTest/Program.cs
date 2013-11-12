03e7e14cd7478a8972fefe4ebb601284b679183e
PoqaTJ <Tbjohnson42@gmail.com>
2013-11-11 19:48:59 -0800

    Added comments.
    Without comments the guy from our XNA class would come and beat me up.

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -9,14 +9,14 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            List<String> strings = new List<String>() { "Git over here!", "The Gittysburg Address", "Gitar Hero" };
-            Random rand = new Random();
+            List<String> strings = new List<String>() { "Git over here!", "The Gittysburg Address", "Gitar Hero" }; // Add strings here with commas between and surrounded by "quotes"!
+            Random rand = new Random(); // This is a Random. I will use it later to do stuff randomly. Spork optional.
 
-            Console.WriteLine("Hello, world!");
-            Console.WriteLine();
+            Console.WriteLine("Hello, world!"); // http://en.wikipedia.org/wiki/Hello_world_program
+            Console.WriteLine(); // writes a blank line
 
-            Console.WriteLine(strings[rand.Next(strings.Count)]);
-            Console.ReadLine();
+            Console.WriteLine(strings[rand.Next(strings.Count)]); // And this is why it's nice that the upper bound for Random.Next() is exclusive.
+            Console.ReadLine(); // This makes it so the console doesn't close right away.
         }
     }
 }
